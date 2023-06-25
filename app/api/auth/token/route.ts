3904f331aedddfbcf65c4692f07d36a0a771bd8b
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-25 21:17:36 +0100

    create the endpoint to handle the getToken request

---
+++ app/api/auth/token/route.ts
@@ -0,0 +1,12 @@
+import { getToken } from 'next-auth/jwt'
+import { NextRequest, NextResponse } from 'next/server'
+
+const secret = process.env.NEXTAUTH_SECRET!
+export async function GET(req: NextRequest) {
+  try {
+    const token = await getToken({ req, secret, raw: true })
+    return NextResponse.json({ token }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 })
+  }
+}
