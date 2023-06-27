302a874f3e2ac43c56a94fe5537b7d8721845433
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-27 02:48:49 +0100

    create the actions to fetch all project and one to fetch user related projects

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,8 @@ import { ProjectForm } from '@/common.types'
 import {
   createProjectMutation,
   createUserMutation,
+  getProjectByIdQuery,
+  getProjectsOfUserQuery,
   getUserQuery,
   projectsQuery,
 } from '@/graphql'
@@ -88,4 +90,13 @@ export const fetchAllProjects = async (
 ) => {
   client.setHeader('x-api-key', apiKey)
   return makeGraphQLRequest(projectsQuery, { category, endcursor })
+}
+
+export const getProjectDetails = async (id: string) => {
+  client.setHeader('x-api-key', apiKey)
+  return makeGraphQLRequest(getProjectByIdQuery, { id })
+}
+export const getUserProjects = async (id: string, last?: number) => {
+  client.setHeader('x-api-key', apiKey)
+  return makeGraphQLRequest(getProjectsOfUserQuery, { id, last })
 }
