b02a8c1e142c0ff1ff644c8a4a573bb77cc91780
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-27 20:04:45 +0100

    fix the name of the nev variable for the app url

--- lib/actions.ts
+++ lib/actions.ts
@@ -18,7 +18,7 @@ const apiKey = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY!
   : 'letmein'
 const serverUrl = isProduction
-  ? process.env.NEXT_PUBLIC_SERVER_URL!
+  ? process.env.NEXTAUTH_URL!
   : 'http://localhost:3000'
 
 const client = new GraphQLClient(apiUrl)
