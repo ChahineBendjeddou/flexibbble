c0ed9f89e80f305927a160bb1ef1b845f7f923d0
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-27 01:54:27 +0100

    display a no project found if no project were created else display the projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,47 @@
-const Home = () => {
+import { ProjectInterface } from '@/common.types'
+import ProjectCard from '@/components/ProjectCard'
+import { fetchAllProjects } from '@/lib/actions'
+type ProjectSearch = {
+  projectSearch: {
+    edges: { node: ProjectInterface }[]
+    pageInfo: {
+      hasPreviousPage: boolean
+      hasNextPage: boolean
+      startCursor: string
+      endCursor: string
+    }
+  }
+}
+
+const Home = async () => {
+  const data = (await fetchAllProjects()) as ProjectSearch
+  const projectsToDisplay = data?.projectSearch?.edges || []
+  if (!projectsToDisplay.length) {
+    return (
+      <section className="flex-col flexStart paddings">
+        Categories
+        <p className="text-center no-result-text">
+          No projects found, go create some first.
+        </p>
+      </section>
+    )
+  }
   return (
     <section className="flex-col mb-16 flex-start paddings">
       <h1>Categories</h1>
-      <h1>Posts</h1>
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={node.id}
+            id={node.id}
+            image={node.image}
+            title={node.title}
+            name={node?.createdBy?.name}
+            avatarUrl={node?.createdBy?.avatarUrl}
+            userId={node?.createdBy?.id}
+          />
+        ))}
+      </section>
       <h1>Loadmore</h1>
     </section>
   )
