220b9cb6bec30a5de1b31b664c6347dffb789836
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-27 02:16:08 +0100

    complete the project card + display some dynamic dummy data for teh likes and views

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,4 +1,7 @@
-import { FC } from 'react'
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC, useEffect, useState } from 'react'
 
 interface ProjectCardProps {
   id: string
@@ -17,7 +20,56 @@ const ProjectCard: FC<ProjectCardProps> = ({
   avatarUrl,
   userId,
 }) => {
-  return <div>ProjectCard</div>
+  const [randomLikes, setRandomLikes] = useState(0)
+  const [randomViews, setRandomViews] = useState('')
+
+  useEffect(() => {
+    setRandomLikes(Math.floor(Math.random() * 10000))
+    setRandomViews((Math.floor(Math.random() * 10000) / 1000).toFixed(1))
+  }, [])
+  return (
+    <div className="flex-col flexCenter rounded-2xl drop-shadow-md">
+      <Link
+        href={`/project/${id}`}
+        className="relative w-full h-full flexCenter group"
+      >
+        <Image
+          src={image}
+          width={414}
+          height={314}
+          alt="Project image"
+          className="object-cover w-full h-full rounded-2xl"
+        />
+        <div className="hidden group-hover:flex profile_card-title">
+          <p className="w-full">{title}</p>
+        </div>
+      </Link>
+      <div className="w-full px-2 mt-3 text-sm font-semibold flexBetween">
+        <Link href={`/profile/${userId}`}>
+          <div className="gap-2 flexCenter">
+            <Image
+              src={avatarUrl}
+              width={24}
+              height={24}
+              className="rounded-full"
+              alt="Profile Image"
+            />
+            <p>{name}</p>
+          </div>
+        </Link>
+        <div className="gap-3 flexCenter">
+          <div className="gap-2 flexCenter">
+            <Image src="/hearth.svg" width={13} height={14} alt="Heart" />
+            <p className="text-sm">{randomLikes}</p>
+          </div>
+          <div className="gap-2 flexCenter">
+            <Image src="/eye.svg" width={13} height={14} alt="Eye" />
+            <p className="text-sm">{randomViews}k</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ProjectCard
